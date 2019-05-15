7ee7d5cf66202bbd915e5e59e9f06e7724dc1fef
Yannik Goldgräbe <ykg@live.de>
2019-05-15 21:56:32 +0200

    Refactoring

--- app.js
+++ app.js
@@ -55,10 +55,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 var store = new BetterMemoryStore({ expires: 60 * 60 * 1000, debug: true });
 app.use(sess({
   name: 'JSESSION',
-  secret: 'MYSECRETISVERYSECRET',
+  secret: config.JSESSION_SECRET,
   store:  store,
   resave: true,
   saveUninitialized: true,
+  cookie : { secure: false }
 }));
 
 app.use(passport.initialize());
--- routes/address.js
+++ routes/address.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Web3 = require('web3');
-var contractAddr = "0x6081aa30758e9D752fd7d8E7729220A80771e835"; 
+var config = require('../config');
 var contr_address = require('../controllers/contr_address'); 
 
 router.get('/', function(req, res, next) {
@@ -16,30 +16,14 @@ router.post('/check', function(req, res){
 
 router.get('/:address', function(req, res, next){
   var userAddr = req.params.address;
-  web3 = new Web3(new Web3.providers.HttpProvider('https://dai.poa.network'));
-  var abi = [{"constant":true,"inputs":[{"name":"_address","type":"address"},{"name":"_label","type":"string"}],"name":"freeBalanceOfLabel","outputs":[{"name":"freeBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_contractAddress","type":"address"}],"name":"addVotingContract","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"},{"name":"_labelID","type":"uint256"}],"name":"labelOfAddress","outputs":[{"name":"label","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"},{"name":"_label","type":"string"},{"name":"_amount","type":"uint256"}],"name":"lockTokens","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"},{"name":"_label","type":"string"}],"name":"balanceOfLabel","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"},{"name":"_label","type":"string"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_label","type":"string"}],"name":"totalLabelSupply","outputs":[{"name":"totalSupplyOfLabel","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"},{"name":"_label","type":"string"},{"name":"_amount","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_address","type":"address"},{"name":"_label","type":"string"},{"name":"_numberOfTokens","type":"uint256"}],"name":"unlockTokens","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"labelCountOfAddress","outputs":[{"name":"labelCount","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"votingContracts","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":false,"name":"label","type":"string"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":false,"name":"label","type":"string"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"}];
-  var ditContract = new web3.eth.Contract(abi, contractAddr);
-
-  var obj;
-  var label = "";
-  var balance = "";
-  contr_address.getProposalsByAddress(userAddr, proposals => {
-    ditContract.methods.labelCountOfAddress(userAddr).call().then(async function(labelCount, error){
-      for(var i = 1; i <= parseInt(labelCount); i++){
-        obj = {};
-        await ditContract.methods.labelOfAddress(userAddr, i).call().then(async function(labelString){
-          label = labelString;
-           await ditContract.methods.balanceOfLabel(userAddr, labelString).call().then(function(labelBalance){
-             balance = web3.utils.toBN(labelBalance).toString();
-             obj[label] = web3.utils.fromWei(balance, 'ether')
-           });
-         });
-       }
-      res.render('address', { user: req.user, address: userAddr, balance: obj, total: sum(obj).toFixed(2), proposals: proposals });
-    }).catch(e => {
+  contr_address.getAddress(userAddr, function(error, result){
+    if(!error){
+      result.user = req.user;
+      res.render('address', result);
+    } else {
       res.render('error-404');
-    });
-  })
+    }
+  });
 });
 
 router.get('/:address/twitterName', function(req, res, next){
@@ -51,19 +35,9 @@ router.get('/:address/twitterName', function(req, res, next){
 
 router.get('/:address/proposals', function(req, res, next){
   var userAddr = req.params.address;
-  contr_address.getProposalsByAddress(userAddr, function(result){
+  contr_address.getProposalsByAddress(userAddr, function(error, result){
     res.send(result);
   });
 });
-
-function sum(obj) {
-  var sum = 0;
-  for(var el in obj) {
-    if(obj.hasOwnProperty(el) ) {
-      sum += parseFloat(obj[el]);
-    }
-  }
-  return sum;
-}
 
 module.exports = router;
