a50cec5f61be6c6f5dc797e1d829a036da721f70
Yannik Goldgräbe <ykg@live.de>
2019-05-15 22:30:33 +0200

    Added error log to database connections

--- models/mdl_generics.js
+++ models/mdl_generics.js
@@ -7,35 +7,47 @@ const dbName = 'twitterbot';
 var model = {
     findAll: function(collection, query, projection, callback){
         MongoClient.connect(url, function(err, client) {
-            const db = client.db(dbName);
-            const col = db.collection(collection);
-            
-            col.find(query).project(projection).toArray(function(err, result) {
-                client.close();
-                callback(err, result);
-            });
+            if (err) {
+                console.error('An error occurred connecting to MongoDB: ', err);
+            } else {
+                const db = client.db(dbName);
+                const col = db.collection(collection);
+                
+                col.find(query).project(projection).toArray(function(err, result) {
+                    client.close();
+                    callback(err, result);
+                });
+            }
         });
     },
     findOne: function(collection, query, projection, callback){
         MongoClient.connect(url, function(err, client) {
-            const db = client.db(dbName);
-            const col = db.collection(collection);
-            
-            col.findOne(query, { projection: projection }, function(err, result) {
-                client.close();
-                callback(err, result);
-            });
+            if (err) {
+                console.error('An error occurred connecting to MongoDB: ', err);
+            } else {
+                const db = client.db(dbName);
+                const col = db.collection(collection);
+                
+                col.findOne(query, { projection: projection }, function(err, result) {
+                    client.close();
+                    callback(err, result);
+                });
+            }
         });
     },
     addNew: function(collection, data, callback){
         MongoClient.connect(url, function(err, client) {
-            const db = client.db(dbName);
-            const col = db.collection(collection);
-            
-            col.insertOne(data, function(err, result) {
-                client.close();
-                callback(err, result);
-            });
+            if (err) {
+                console.error('An error occurred connecting to MongoDB: ', err);
+            } else {
+                const db = client.db(dbName);
+                const col = db.collection(collection);
+                
+                col.insertOne(data, function(err, result) {
+                    client.close();
+                    callback(err, result);
+                });
+            }
         });
     }
 }
