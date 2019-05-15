c1e9afa034c69ff2edfbfc9736a5c7b56a69aa75
Yannik Goldgräbe <ykg@live.de>
2019-05-15 20:29:43 +0200

    Added seperate page for all proposals

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var addressRouter = require('./routes/address');
 var contr_address = require('./controllers/contr_address');
 var loginRouter = require('./routes/login');
 var logoutRouter = require('./routes/logout');
+var proposalsRouter = require('./routes/proposals');
 
 passport.use(new TwitterStrategy({
   consumerKey:    config.TWITTER_API_KEY,
@@ -67,6 +68,7 @@ app.use('/', indexRouter);
 app.use('/address', addressRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
+app.use('/proposals', proposalsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/proposals.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var contr_proposals = require('../controllers/contr_proposals'); 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  contr_proposals.getAllProposals(function(proposals){
+    res.render('proposals', { user: req.user, proposals: proposals });
+  });
+});
+
+module.exports = router;
