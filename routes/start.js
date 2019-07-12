55c9559a16a0f980efa50316a67b65f52bb73dfa
Yannik Goldgräbe <ykg@live.de>
2019-07-12 16:18:14 +0200

    Restricted start page to logged in users

--- routes/start.js
+++ routes/start.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('start', { user: req.user });
+  if(req.user && req.user.eth_address){
+    res.render('start', { user: req.user });
+  } else {
+    res.render('error-404');
+  }
 });
 
 module.exports = router;
