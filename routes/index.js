5b14474259db421678e01524c375225e0440e08c
Yannik Goldgräbe <ykg@live.de>
2019-08-16 17:36:00 +0200

    Added login page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/welcome', function(req, res, next) {
   res.render('index', { user: req.user });
+});
+
+router.get('/', function(req, res, next) {
+  if(req.user){
+    res.render('index', { user: req.user });
+  } else {
+    res.render('login');
+  }
 });
 
 module.exports = router;
